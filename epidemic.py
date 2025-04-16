9d7d9fa1d4247d246795a31e9412be66e9feaca8
ttavera234 <ttavera234@gmail.com>
2025-04-16 17:47:06 -0400

    Created GitHub repository, started basic epidemic structure, and handled csv input.

---
+++ epidemic.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+import csv
+
+
+@dataclass
+class EpidemicData:
+    start_date: str
+    end_date: str
+    grouping: str
+    year: str
+    month: str
+    state: str
+    sex: str
+    age_group: str
+    covid_deaths: str
+    pneumonia_deaths: str
+    influenza_deaths: str
+
+
+class Epidemic:
+    def __init__(self, name: str, month: str, year: str, sex_category: str, age_group: str, death_count: str):
+        self._name: str = name
+        self._month: str = month
+        self._year: str = year
+        self._sex_category: str = sex_category
+        self._age_group: str = age_group
+        self._death_count: int = int(death_count)
+
+
+def handle_csv_data(file_name: str) -> Epidemic:
+    """
+    Description:
+        ...
+
+    Args:
+        file_name: ...
+
+    Returns:
+        ...
+    """
+
+    # From GeeksForGeeks (https://www.geeksforgeeks.org/reading-csv-files-in-python/)
+    with open(file_name, mode = "r") as file:
+        csv_file = csv.DictReader(file)
+        for lines in csv_file:
+            print(lines)
+
+
+handle_csv_data("covid.csv")
