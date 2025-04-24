5128c60258eb41c5f2f5e25b81056f7b1291dc15
ttavera234 <ttavera234@gmail.com>
2025-04-23 21:41:41 -0400

    Implemented basic setup for querying user data for later visual analysis.

--- epidemic.py
+++ epidemic.py
@@ -27,7 +27,7 @@ class Epidemic:
         self._death_count: int = int(death_count)
 
 
-def handle_csv_data(file_name: str) -> None:
+def load_data(start_year: str, end_year: str, epidemic: str, by_nation: bool, state_list: list[str], age_group: tuple[str] = "A", file_name: str = "Covid.csv") -> None:
     """
     Description:
         ...
@@ -42,8 +42,50 @@ def handle_csv_data(file_name: str) -> None:
     # From GeeksForGeeks (https://www.geeksforgeeks.org/reading-csv-files-in-python/)
     with open(file_name, mode = "r") as file:
         csv_file = csv.DictReader(file)
+        obj: dict = {
+            '2023 Covid Deaths': 0
+        }
         for lines in csv_file:
-            print(lines)
+            if lines['Year'] == '2023' and lines['COVID-19 Deaths']:
+                obj['2023 Covid Deaths'] += int(lines['COVID-19 Deaths'])
+            #obj['Date'] = lines['Data As Of']
+            #print(lines)
+        print(obj)
+
+        #print(obj)
 
 
-handle_csv_data("covid.csv")
+def query_data() -> None:
+    """
+        Description:
+            ...
+
+        Args:
+            ...
+
+        Returns:
+            ...
+        """
+
+    # Handling user input -> https://www.geeksforgeeks.org/taking-multiple-inputs-from-user-in-python/
+    time_range: list[str] = input(
+        "Enter the range of years you wish to study in the following format 'YYYY-YYYY': ").split("-")
+    epidemic: str = input(
+        "Enter the epidemic you wish to study in the following format 'C' for COVID-19, 'I' for Influenza, or 'P' for Pneumonia: ")
+    graph_decision: str = input(
+        "Enter 'A' if you wish to study a national graph and 'B' if you wish to study a state graph: ")
+    if graph_decision == 'A':
+        list_of_states: list[str] = input(
+            "Enter two or more states you wish to study in the following comma-separated format 'CA, FL, MI': ").split(", ")
+        load_data()
+    else:
+        state: str = input(
+            "Enter the state you wish to study in the following format 'FL' : ")
+        group_decision: str = input(
+            "Enter 'G' if you wish to focus on gender or 'A' if you wish to focus on a specified age group: ")
+        if group_decision == 'A':
+            age_groups: list[str] = input(
+                "Enter the age groups you wish to study in the following comma-separated format '0-17, 18-29, 30-49, 50-64, 65-84': ").split(", ")
+
+
+query_data()
